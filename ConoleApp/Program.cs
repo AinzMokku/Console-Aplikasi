86b37ff1502a65785638db4b1ab7c29359681a75
AinzMokku <ainz.mokku@.gmail.com>
2018-08-31 10:25:37 +0700

    perubahan terbaru

--- ConoleApp/Program.cs
+++ ConoleApp/Program.cs
@@ -42,6 +42,8 @@ namespace ConoleApp
             MyPro.tuliswarna(6, 8, menu[pilih], ConsoleColor.DarkYellow, ConsoleColor.DarkBlue);
 
             ConsoleKeyInfo tombol;
+            bool mainmenu = true;
+
             do
             {
                 tombol = Console.ReadKey(true);
@@ -55,7 +57,9 @@ namespace ConoleApp
                     else { pilih++; };
                     MyPro.tuliswarna(6, 8 + pilih, menu[pilih], ConsoleColor.DarkYellow, ConsoleColor.DarkBlue);
 
-                }else if (tombol.Key == ConsoleKey.UpArrow)
+                }
+                
+                if (tombol.Key == ConsoleKey.UpArrow)
                 {
                     MyPro.tuliswarna(6, 8 + pilih, menu[pilih], ConsoleColor.White, ConsoleColor.Black);
                     if (menu[pilih] == menu[0])
@@ -66,44 +70,73 @@ namespace ConoleApp
                     MyPro.tuliswarna(6, 8 + pilih, menu[pilih], ConsoleColor.DarkYellow, ConsoleColor.DarkBlue);
                 }
 
+                if (tombol.Key == ConsoleKey.LeftArrow)
+                {
+                    mainmenu = false;
+                }
+
                 if (tombol.Key == ConsoleKey.RightArrow)
                     {
                         if (menu[pilih] == menu[0])
                         {
-                            
-                            List<string[]> siswa = new List<string[]>();
-                            string jawaban;
-                            do
-                            {
-                                string[] data = new string[5];
-                                MyPro.tulis(22, 7, "Input data Guru ");
-                                MyPro.tulis(22, 8, "NIM      : ");
-                                data[0] = Console.ReadLine();
-                                MyPro.tulis(22, 9, "NAMA     : ");
-                                data[1] = Console.ReadLine();
-                                MyPro.tulis(22, 10, "KELAS    : ");
-                                data[2] = Console.ReadLine();
-                                MyPro.tulis(22, 11, "ALAMAT   : ");
-                                data[3] = Console.ReadLine();
-                                MyPro.tulis(22, 12, "AGAMA    : ");
-                                data[4] = Console.ReadLine();
-                                siswa.Add(data);
-                                System.Threading.Thread.Sleep(25);
+                                Console.SetCursorPosition(22,8);
+                                string[] pilihansiswa = new string[4];
+                                pilihansiswa[0] = "TAMBAH DATA SISWA";
+                                pilihansiswa[1] = "TAMPIL DATA SISWA";
+                                pilihansiswa[2] = "EDIT DATA SISWA";
+                                pilihansiswa[3] = "HAPUS DATA SISWA";
+                                
 
-
-                                MyPro.tulis(22, 13, "Apakah anda masih mau menambah data siswa [Y/N]");
-                                jawaban = Console.ReadLine();
-                                for (int a = 1; a <= 7; a++)
+                                for (int a = 0; a < 4; a++)
                                 {
-                                    MyPro.tulis(22, 6 + a, "                                                                    ");
+                                    MyPro.tulis(22, 8 + a, pilihansiswa[a]);
                                 }
-                                Console.ForegroundColor = ConsoleColor.Black;
-                                Console.ForegroundColor = ConsoleColor.White;
-                                Console.BackgroundColor = ConsoleColor.Black;
-                            } while (jawaban.ToUpper() == "Y");
+                                int psiswa = 0;       
+                                MyPro.tuliswarna(22, 8, pilihansiswa[psiswa], ConsoleColor.DarkYellow, ConsoleColor.DarkBlue);
+                                do
+                                    {
+                                        tombol = Console.ReadKey(true);
+                                        if (tombol.Key == ConsoleKey.DownArrow)
+                                        {
+                                            MyPro.tuliswarna(22, 8 + psiswa, pilihansiswa[psiswa], ConsoleColor.White, ConsoleColor.Black);
+                                            if (pilihansiswa[psiswa] == pilihansiswa[3])
+                                            {
+                                                psiswa = 0;
+                                            }
+                                            else { psiswa++; };
+                                            MyPro.tuliswarna(22, 8 + psiswa, pilihansiswa[psiswa], ConsoleColor.DarkYellow, ConsoleColor.DarkBlue);
+
+                                        }
+                
+                                        if (tombol.Key == ConsoleKey.UpArrow)
+                                        {
+                                            MyPro.tuliswarna(22, 8 + psiswa, pilihansiswa[psiswa], ConsoleColor.White, ConsoleColor.Black);
+                                            if (pilihansiswa[psiswa] == pilihansiswa[0])
+                                            {
+                                                psiswa = 3;
+                                            }
+                                            else { psiswa--; };
+                                            MyPro.tuliswarna(22, 8 + psiswa, pilihansiswa[psiswa], ConsoleColor.DarkYellow, ConsoleColor.DarkBlue);
+                                        }
+
+                                        if (tombol.Key == ConsoleKey.RightArrow)
+                                        {
+                                            MyPro.tuliswarna(22, 8 + psiswa, pilihansiswa[psiswa], ConsoleColor.White, ConsoleColor.Black);
+                                            if (pilihansiswa[psiswa] == pilihansiswa[0])
+                                            {
+                                                psiswa = 3;
+                                            }
+                                            else { psiswa++; };
+                                            MyPro.tuliswarna(22, 8 + psiswa, pilihansiswa[psiswa], ConsoleColor.DarkYellow, ConsoleColor.DarkBlue);
+                                        }
+                                    } while (tombol.Key != ConsoleKey.LeftArrow);
+                        
+
+
+                               
                         }
                     }
-            } while (tombol.Key != ConsoleKey.Enter);
+            } while (mainmenu);
 
                
 
